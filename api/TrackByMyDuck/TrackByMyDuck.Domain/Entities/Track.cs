b4cd3adcf009e01df7abfb39b88021fbd4c7a888
jachumail <jachumail@yahoo.pl>
2022-10-24 23:50:36 +0200

    Entities

---
+++ api/TrackByMyDuck/TrackByMyDuck.Domain/Entities/Track.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackByMyDuck.Domain.Entities
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public Guid SpotifyId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ api/TrackByMyDuck/TrackByMyDuck.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackByMyDuck.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string SpotifyId { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+    }
+}
