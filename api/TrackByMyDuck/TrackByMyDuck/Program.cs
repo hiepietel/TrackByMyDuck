4bc15070cb1dd725697f72d43bf775304ef4cc10
jachumail <jachumail@yahoo.pl>
2022-11-06 15:28:57 +0100

    spotify playlist controller

--- api/TrackByMyDuck/TrackByMyDuck/Program.cs
+++ api/TrackByMyDuck/TrackByMyDuck/Program.cs
@@ -1,14 +1,43 @@
-//using SpotifyAPI.Web;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration;
 var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
-// Add services to the container.
+
+
 builder.Services.AddControllers();
-//init
-//builder.Services.AddSingleton(SpotifyClientConfig.CreateDefault());
-//builder.Services.AddScoped<SpotifyClientBuilder>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)
+            ),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: myAllowSpecificOrigins,
@@ -23,6 +52,8 @@ var app = builder.Build();
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseCors(myAllowSpecificOrigins);
 app.UseHttpsRedirection();
