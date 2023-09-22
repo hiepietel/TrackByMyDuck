64c16b4f76a05439dc3c53d8fdb80431a25b0402
jachumail <jachumail@yahoo.pl>
2023-09-22 13:49:09 +0200

    2

--- front/src/components/Auth/Login.tsx
+++ front/src/components/Auth/Login.tsx
@@ -47,7 +47,7 @@ const Login: React.FC = () => {
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" component="h4">
-        Track by my duck :D
+        Track by my duck :DD
       </Typography>
 
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
